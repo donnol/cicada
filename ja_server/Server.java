239e26d45d3a2ba8af3d85a093183eb27cdeca34
jdlau <jdlau@qq.com>
2018-04-19 22:05:22 +0800

    update ja_server

--- ja_server/Server.java
+++ ja_server/Server.java
@@ -1,4 +1,3 @@
-package com.jdlau.main;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,11 +8,11 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
-class Server {
+public class Server {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8820), 0);
-        server.createContext("/applications/myapp", new MyHandler());
-        server.setExecutor(null); // creates a default executor
+        server.createContext("/hello", new MyHandler());
+        server.setExecutor(null);
         server.start();
     }
 
@@ -23,8 +22,7 @@ class MyHandler implements HttpHandler {
     public void handle(HttpExchange t) throws IOException {
         InputStream is = t.getRequestBody();
         System.out.println(is);
-        // read(is); // .. read the request body
-        String response = "This is the response";
+        String response = "Hello, I am jd";
         t.sendResponseHeaders(200, response.length());
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes());
---
+++ ja_server/make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/mnt/c/Java/jdk-10/bin/javac.exe Server.java &&
+java.exe Server
