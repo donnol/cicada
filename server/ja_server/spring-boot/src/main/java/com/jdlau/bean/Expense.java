b66998dbe8c5ff3fa8ad1e21196b792247b69b29
jdlau <jdlau@qq.com>
2018-04-28 22:03:21 +0800

    ja_server 完成数据读取和json返回

--- server/ja_server/spring-boot/src/main/java/com/jdlau/bean/Expense.java
+++ server/ja_server/spring-boot/src/main/java/com/jdlau/bean/Expense.java
@@ -1,5 +1,6 @@
 package com.jdlau.bean;
 
+import java.sql.Time;
 import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,9 +16,13 @@ public class Expense {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private int user_id;
+    private Long user_id;
     private String user_name;
     private float pay;
+    private String thing;
+    private Time created_at;
+    private String created_on;
+    private Time updated_at;
 
     public Expense() {
     }
@@ -30,11 +35,59 @@ public class Expense {
         this.id = id;
     }
 
+    public Long getUserID() {
+        return user_id;
+    }
+
+    public void setUserID(Long userID) {
+        this.user_id = userID;
+    }
+
     public String getUserName() {
         return user_name;
     }
 
     public void setUserName(String name) {
         this.user_name = name;
+    }
+
+    public float getPay() {
+        return pay;
+    }
+
+    public void setPay(float pay) {
+        this.pay = pay;
+    }
+
+    public String getThing() {
+        return thing;
+    }
+
+    public void setThing(String thing) {
+        this.thing = thing;
+    }
+
+    public String getCreatedOn() {
+        return created_on;
+    }
+
+    public void setCreatedOn(String co) {
+        this.created_on = co;
+    }
+
+    public Time getCreatedAt() {
+        return created_at;
+    }
+
+    public void setCreatedAt(Time ca) {
+        this.created_at = ca;
+    }
+
+    public Time getUpdatedAt() {
+        return updated_at;
+    }
+
+    public void setUpdatedAt(Time ua) {
+        this.updated_at = ua;
     }
 }
--- server/ja_server/spring-boot/src/main/java/com/jdlau/controller/ExpenseController.java
+++ server/ja_server/spring-boot/src/main/java/com/jdlau/controller/ExpenseController.java
@@ -5,8 +5,8 @@ import com.jdlau.service.IExpenseService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class ExpenseController {
@@ -14,14 +14,11 @@ public class ExpenseController {
     @Autowired
     IExpenseService expenseService;
 
-    @RequestMapping("/showExpenses")
-    public String findExpenses(Model model) {
+    @RequestMapping(value = "/showExpenses", produces = "application/json")
+    public @ResponseBody List<Expense> findExpenses() {
 
         List<Expense> expenses = (List<Expense>) expenseService.findAll();
 
-        System.out.println(expenses);
-        model.addAttribute("expenses", expenses);
-
-        return "showExpenses";
+        return expenses;
     }
 }
