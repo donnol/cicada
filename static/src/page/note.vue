a1276cb8ee967ed7a45b314540b0af59e7ce424e
jdlau <jdlau@qq.com>
2018-06-23 15:35:33 +0800

    添加提醒

--- static/src/page/note.vue
+++ static/src/page/note.vue
@@ -20,11 +20,21 @@ export default {
       axios
         .post('/AddNote', params) // 不是simple request的话，会将post方法变为options。使用 URLSearchParams 可以避免
         .then(response => {
+          this.open('笔记', '提交成功')
           console.log(response)
         })
         .catch(error => {
+          this.open('笔记', '提交失败: ' + error)
           console.log(error)
         })
+    },
+    open(title, message) {
+      const h = this.$createElement
+
+      this.$notify({
+        title: title,
+        message: h('i', { style: 'color: teal' }, message)
+      })
     }
   },
   data() {
